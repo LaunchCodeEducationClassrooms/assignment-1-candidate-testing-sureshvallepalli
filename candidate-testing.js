08d417e9c0f917bd38ef79397cfc564802a9d3d4
Suresh Vallepalli <sureshvallepalli@gmail.com>
2021-02-21 05:22:47 +0000

    I am able to generate the output that is expected

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,30 +8,41 @@ let candidateName;
 let question;
 let correctAnswer;
 let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
-
+let questions = ["Who was the first American woman in space? ","True or false: 5000 meters = 5 kilometers. ","(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS? "];
+let correctAnswers= ["Sally Ride","True","40","Trajectory","3"];
+let candidateAnswers = [];
+let result = 0;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
+  candidateName = input.question("Enter your name: ");
+  console.log(candidateName);
 
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+for (let i = 0; i < questions.length; i++) {
+   candidateAnswers[i] = input.question(questions[i]);
+}
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+  console.log("\n\n-------------------------Output-------------------------------\n");
+  console.log("Candidate Name: " + candidateName); 
+  for (let i = 0; i < questions.length; i++) {
+    console.log(questions[i]);
+    console.log("Your Answer: "+candidateAnswers[i]);
+    console.log("Correct Answer: "+correctAnswers[i]+"\n");
+    if(candidateAnswers[i].toLowerCase() == correctAnswers[i].toLowerCase()){
+        result++;
+    }
+  }
 
-
-  let grade;
-  
-
+  grade = 100*result/questions.length;
+ 
   return grade;
 }
 
@@ -40,7 +51,16 @@ function runProgram() {
   // TODO 1.1c: Ask for candidate's name //
   
   askQuestion();
-  gradeQuiz(this.candidateAnswers);
+
+  let status;
+  let quizResult = gradeQuiz(this.candidateAnswers);
+
+   if(quizResult >= 80){
+    status = "PASSED";
+  } else {
+    status = "FAILED";
+  }
+  console.log(">>> Overall Grade: " + quizResult + "%" + " ("+result+" of 5 responses correct) <<<\n"+">>> Status: "+status+" <<<\n\n");
 }
 
 // Don't write any code below this line //
